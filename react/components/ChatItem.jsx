7e835d8a472f8a8b54ce062c86734b782c6631a1
yumekiti <yumekiti120440@gmail.com>
2022-03-01 01:31:06 +0900

    fix ws

--- react/components/ChatItem.jsx
+++ react/components/ChatItem.jsx
@@ -1,10 +1,10 @@
 import { React } from '../../deno/deps.ts';
 import { twind } from '../../deno/deps.ts';
 
-export default ({ id, body }) => {
+export default ({ myid, id, body }) => {
     return (
-        <li class={id == id ? twind.tw`flex justify-start` : twind.tw`flex justify-end`}>
-            <div class={id == id ? twind.tw`relative max-w-xl px-4 py-2 text-gray-700 rounded shadow` : twind.tw`relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow`}>
+        <li class={id == myid ? twind.tw`flex justify-start` : twind.tw`flex justify-end`}>
+            <div class={id == myid ? twind.tw`relative max-w-xl px-4 py-2 text-gray-700 rounded shadow` : twind.tw`relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow`}>
                 <span class={twind.tw`text-xs`}>id : { id }</span>
                 <span class={twind.tw`block`}>{ body }</span>
             </div>
