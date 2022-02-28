b79ab9077f31b2aeeeabc2cd72ec6a130d2b7517
yumekiti <yumekiti120440@gmail.com>
2022-02-28 23:55:33 +0900

    del: message usestate

--- react/app.jsx
+++ react/app.jsx
@@ -6,8 +6,6 @@ import ChatList from './components/ChatList.jsx';
 const App = () => {
     const client = new WebSocket("ws://localhost:8080/ws");
 
-    const [chats, setChats] = React.useState([]);
-    const [message, setMessage] = React.useState([]);
     const [id, setId] = React.useState(null);
 
     const uuid = () => {
@@ -28,27 +26,8 @@ const App = () => {
         uuid()
     }
 
-    const handleChange = (event) => {
-        setMessage(event.target.value);
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const data = {
-            id: id,
-            body: message,
-        };
-        client.send(JSON.stringify(data));
-        setMessage('');
-    }
-
-    client.addEventListener("message", ({data}) => {
-        let json = JSON.parse(data);
-        setChats((chats) => [...chats, {id: json.id, body: json.body} ]);
-    })
-
     return (
-        <ChatList chats={chats} message={message} handleChange={handleChange} handleSubmit={handleSubmit} />
+        <ChatList id={id} client={client} />
     )
 };
   
